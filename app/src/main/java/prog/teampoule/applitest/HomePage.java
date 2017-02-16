655a0579e561a562279fe5762f6ee0bbc84dfdb1
ombrecube <massj.info@gmail.com>
2017-02-16 10:35:46 -0500

    Premier commit
    Initialize

---
+++ app/src/main/java/prog/teampoule/applitest/HomePage.java
@@ -0,0 +1,13 @@
+package prog.teampoule.applitest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
